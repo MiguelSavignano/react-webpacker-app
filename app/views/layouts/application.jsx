67b0a7fc5e73c7679bd865e9c25a956440696460
Miguel Savignano <migue.masx@gmail.com>
2018-04-14 20:45:09 +0200

    .

---
+++ app/views/layouts/application.jsx
@@ -0,0 +1,4 @@
+import React from "react"
+
+const ApplicationLayout = props => <div>ApplicationLayout{props.children}</div>
+export default ApplicationLayout
---
+++ app/views/posts/new.jsx
@@ -0,0 +1,4 @@
+import React from "react"
+
+const PostsNewView = () => <h1>PostsNewView posts#edit</h1>
+export default PostsNewView
